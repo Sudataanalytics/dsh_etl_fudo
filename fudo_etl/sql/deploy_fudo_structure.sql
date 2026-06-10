2d33fe2ef3c6f7e18318b7e7bac38245ec80c7d2
JoaquinRamirez98 <joaquin.ramirez170998@gmail.com>
2026-06-10 15:49:02 -0300

    sucursal policastro agregada

--- fudo_etl/sql/deploy_fudo_structure.sql
+++ fudo_etl/sql/deploy_fudo_structure.sql
@@ -97,6 +97,16 @@ ON CONFLICT (id_sucursal) DO UPDATE SET
     secret_manager_apikey_name = EXCLUDED.secret_manager_apikey_name, secret_manager_apisecret_name = EXCLUDED.secret_manager_apisecret_name,
     updated_at = CURRENT_TIMESTAMP;
 
+-- Añadir la nueva sucursal 'nani_panaderia'
+INSERT INTO public.config_fudo_branches (id_sucursal, fudo_branch_identifier, sucursal_name, secret_manager_apikey_name, secret_manager_apisecret_name)
+VALUES ('policastro', 'MTNAMzU2MjQ1', 'Policastro', 'FUDO_POLICASTRO_APIKEY', 'FUDO_POLICASTRO_APISECRET')
+ON CONFLICT (id_sucursal) DO UPDATE SET
+    fudo_branch_identifier = EXCLUDED.fudo_branch_identifier, 
+    sucursal_name = EXCLUDED.sucursal_name,
+    secret_manager_apikey_name = EXCLUDED.secret_manager_apikey_name, 
+    secret_manager_apisecret_name = EXCLUDED.secret_manager_apisecret_name,
+    updated_at = CURRENT_TIMESTAMP;
+
 
 -- ----------------------------------------------------------------------
 -- 2. TABLAS PARA LA CAPA DE DATOS CRUDOS (RAW LAYER)
